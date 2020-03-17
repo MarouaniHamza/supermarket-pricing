1b7538d7c94b277b9be1d8faca38eaf7a307107e
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 20:58:09 +0100

    [TDD-RED] nominal case for pricing by weight : one item provided

--- src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
@@ -2,9 +2,14 @@ package org.supermarket.pricing;
 
 import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
+import org.supermarket.pricing.model.Item;
 import org.supermarket.pricing.scheme.PricingByWeightScheme;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class PricingByWeightSchemeTest {
 
@@ -18,5 +23,15 @@ public class PricingByWeightSchemeTest {
     @Test(expected = NoItemProvidedException.class)
     public void should_throw_NoItemProvidedException_if_empty_list(){
         pricingByWeightScheme.computePrice(Collections.emptyList());
+    }
+
+    @Test
+    public void should_return_price_multiplied_by_weight_when_one_item_provided(){
+        //given
+        List<Item> items = Collections.singletonList(new Item("apples_bag", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.8537)));
+        //when
+        BigDecimal price = pricingByWeightScheme.computePrice(items);
+        //then
+        assertEquals(BigDecimal.valueOf(0.75 * 0.8537), price);
     }
 }
