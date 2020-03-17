16063e07b887ec647a4742cab07976ac1541a451
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 20:51:14 +0100

    [TDD-RED] nominal case for pricing by weight : no item provided

--- src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
 import org.supermarket.pricing.scheme.PricingByWeightScheme;
 
+import java.util.Collections;
+
 public class PricingByWeightSchemeTest {
 
     private final PricingByWeightScheme pricingByWeightScheme = new PricingByWeightScheme();
@@ -11,5 +13,10 @@ public class PricingByWeightSchemeTest {
     @Test(expected = NoItemProvidedException.class)
     public void should_throw_NoItemProvidedException_if_null(){
         pricingByWeightScheme.computePrice(null);
+    }
+
+    @Test(expected = NoItemProvidedException.class)
+    public void should_throw_NoItemProvidedException_if_empty_list(){
+        pricingByWeightScheme.computePrice(Collections.emptyList());
     }
 }
