1555ed8e7678eb4996f8810af25526fc696b8a72
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 15:31:25 +0100

    [TDD-RED] nominal case for pricing by number : one item provided

--- src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
@@ -2,10 +2,15 @@ package org.supermarket.pricing;
 
 import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
+import org.supermarket.pricing.model.Item;
 import org.supermarket.pricing.scheme.PricingByNumberScheme;
 import org.supermarket.pricing.scheme.PricingScheme;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class PricingByNumberSchemeTest {
 
@@ -19,5 +24,15 @@ public class PricingByNumberSchemeTest {
     @Test(expected = NoItemProvidedException.class)
     public void should_throw_NoItemProvidedException_if_empty_list(){
         pricingByNumberScheme.computePrice(Collections.emptyList());
+    }
+
+    @Test
+    public void should_return_one_price_when_one_item_provided(){
+        //given
+        List<Item> items = Collections.singletonList(new Item("soda_bottle", BigDecimal.valueOf(1.5), BigDecimal.valueOf(0.7)));
+        //when
+        BigDecimal price = pricingByNumberScheme.computePrice(items);
+        //then
+        assertEquals(price, BigDecimal.valueOf(1.5));
     }
 }
