39e9b7d42ce7a29b83e8330b36554b33948c773c
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 16:09:58 +0100

    [TDD-RED] nominal case for pricing by number : too many items provided and price not rounded

--- src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
@@ -7,6 +7,8 @@ import org.supermarket.pricing.scheme.PricingByNumberScheme;
 import org.supermarket.pricing.scheme.PricingScheme;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -38,7 +40,7 @@ public class PricingByNumberSchemeTest {
     }
 
     @Test
-    public void should_return_price_multiplied_by_price_when_many_items_provided(){
+    public void should_return_price_multiplied_by_number_when_many_items_provided(){
         //given
         List<Item> items = Arrays.asList(new Item("soda_bottle", BigDecimal.valueOf(1.5), BigDecimal.valueOf(0.7)),
                 new Item("soda_bottle", BigDecimal.valueOf(1.5), BigDecimal.valueOf(0.7)),
@@ -48,5 +50,18 @@ public class PricingByNumberSchemeTest {
         BigDecimal price = pricingByNumberScheme.computePrice(items);
         //then
         assertEquals(BigDecimal.valueOf(1.5 * 4), price);
+    }
+
+    @Test
+    public void should_return_price_multiplied_by_number_rounded_to_two_digit_when_too_many_items_provided(){
+        //given
+        List<Item> items = new ArrayList<>();
+        for(int i = 0 ; i < 16419; i++){
+            items.add(new Item("soda_bottle", BigDecimal.valueOf(1.231), BigDecimal.valueOf(0.7)));
+        }
+        //when
+        BigDecimal price = pricingByNumberScheme.computePrice(items);
+        //then
+        assertEquals(BigDecimal.valueOf(1.231 * 16419).setScale(2, RoundingMode.CEILING), price);
     }
 }
