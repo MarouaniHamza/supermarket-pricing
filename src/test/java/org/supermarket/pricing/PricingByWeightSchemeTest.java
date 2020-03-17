aea90476ad43da4ba3c125b72fee19477ca3a205
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 21:13:59 +0100

    [TDD-RED] nominal case for pricing by weight : many items provided

--- src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByWeightSchemeTest.java
@@ -7,6 +7,7 @@ import org.supermarket.pricing.scheme.PricingByWeightScheme;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -34,5 +35,19 @@ public class PricingByWeightSchemeTest {
         BigDecimal price = pricingByWeightScheme.computePrice(items);
         //then
         assertEquals(BigDecimal.valueOf(0.75 * 0.8537).setScale(2, RoundingMode.UP), price);
+    }
+
+    @Test
+    public void should_return_sum_of_price_multiplied_by_weight_when_many_items_provided(){
+        //given
+        List<Item> items = Arrays.asList(new Item("apples_bag", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.8537)),
+                new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.4252)),
+                new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(1.2071)),
+                new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(2.7505)));
+        //when
+        BigDecimal price = pricingByWeightScheme.computePrice(items);
+        BigDecimal expectedPrice = BigDecimal.valueOf(0.75 *(0.8537+0.4252+1.2071+2.7505));
+        //then
+        assertEquals(expectedPrice.setScale(2, RoundingMode.UP), price);
     }
 }
