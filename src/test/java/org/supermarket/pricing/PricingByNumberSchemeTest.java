de693b1dbc6b6a242dcbfd3ecbe3b3438ab6baa2
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-17 15:07:08 +0100

    [TDD-RED] nominal case for pricing by number : no item provided

--- src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByNumberSchemeTest.java
@@ -2,8 +2,11 @@ package org.supermarket.pricing;
 
 import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
+import org.supermarket.pricing.model.Item;
 import org.supermarket.pricing.scheme.PricingByNumberScheme;
 import org.supermarket.pricing.scheme.PricingScheme;
+
+import java.util.ArrayList;
 
 public class PricingByNumberSchemeTest {
 
@@ -12,5 +15,10 @@ public class PricingByNumberSchemeTest {
     @Test(expected = NoItemProvidedException.class)
     public void should_throw_NoItemProvidedException_if_null(){
         pricingByNumberScheme.computePrice(null);
+    }
+
+    @Test(expected = NoItemProvidedException.class)
+    public void should_throw_NoItemProvidedException_if_empty_list(){
+        pricingByNumberScheme.computePrice(new ArrayList<>());
     }
 }
