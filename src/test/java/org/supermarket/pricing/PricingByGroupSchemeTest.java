cb2a5529eab43460ec0432098888542931e5c743
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-18 13:38:45 +0100

    [TDD-RED] Nominal case of Group scheme : no item provided

--- src/test/java/org/supermarket/pricing/PricingByGroupSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByGroupSchemeTest.java
@@ -2,7 +2,10 @@ package org.supermarket.pricing;
 
 import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
+import org.supermarket.pricing.scheme.PricingByDiscountScheme;
 import org.supermarket.pricing.scheme.PricingByGroupScheme;
+
+import java.util.Collections;
 
 public class PricingByGroupSchemeTest {
 
@@ -12,5 +15,11 @@ public class PricingByGroupSchemeTest {
     public void should_throw_NoItemProvidedException_if_null() {
         pricingByGroupScheme = new PricingByGroupScheme();
         pricingByGroupScheme.computePrice(null);
+    }
+
+    @Test(expected = NoItemProvidedException.class)
+    public void should_throw_NoItemProvidedException_if_empty_list() {
+        pricingByGroupScheme = new PricingByGroupScheme();
+        pricingByGroupScheme.computePrice(Collections.emptyList());
     }
 }
