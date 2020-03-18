77e7981b51eb5daacca7487a5f056a05bd5bd57c
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-18 11:23:16 +0100

    [TDD-RED] nominal case for pricing by discount : for 3 items, one is free

--- src/test/java/org/supermarket/pricing/PricingByDiscountSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByDiscountSchemeTest.java
@@ -2,9 +2,15 @@ package org.supermarket.pricing;
 
 import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
+import org.supermarket.pricing.model.Item;
 import org.supermarket.pricing.scheme.PricingByDiscountScheme;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class PricingByDiscountSchemeTest {
 
@@ -20,6 +26,19 @@ public class PricingByDiscountSchemeTest {
     public void should_throw_NoItemProvidedException_if_empty_list() {
         pricingByDiscountScheme = new PricingByDiscountScheme();
         pricingByDiscountScheme.computePrice(Collections.emptyList());
+    }
+
+    @Test
+    public void should_return_price_of_two_if_buy_three(){
+        //given
+        pricingByDiscountScheme = new PricingByDiscountScheme();
+        List<Item> items = Arrays.asList(new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.45)),
+                new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.45)),
+                new Item("soda_bottle", BigDecimal.valueOf(0.75), BigDecimal.valueOf(0.45)));
+        //when
+        BigDecimal price = pricingByDiscountScheme.computePrice(items);
+        //then
+        assertEquals(BigDecimal.valueOf(0.75 * 2),price);
     }
 
 }
