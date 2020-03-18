3852749beafdfe8d32f861c569561d4864a3cd3a
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-18 11:02:42 +0100

    [TDD-RED] nominal case for pricing by discount : no item provided

--- src/test/java/org/supermarket/pricing/PricingByDiscountSchemeTest.java
+++ src/test/java/org/supermarket/pricing/PricingByDiscountSchemeTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.supermarket.pricing.exception.NoItemProvidedException;
 import org.supermarket.pricing.scheme.PricingByDiscountScheme;
 
+import java.util.Collections;
+
 public class PricingByDiscountSchemeTest {
 
     private PricingByDiscountScheme pricingByDiscountScheme;
@@ -12,6 +14,12 @@ public class PricingByDiscountSchemeTest {
     public void should_throw_NoItemProvidedException_if_null() {
         pricingByDiscountScheme = new PricingByDiscountScheme();
         pricingByDiscountScheme.computePrice(null);
+    }
+
+    @Test(expected = NoItemProvidedException.class)
+    public void should_throw_NoItemProvidedException_if_empty_list() {
+        pricingByDiscountScheme = new PricingByDiscountScheme();
+        pricingByDiscountScheme.computePrice(Collections.emptyList());
     }
 
 }
