18d55c093cb72212d482450762e5e932a946385e
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-18 11:00:44 +0100

    [TDD-GREEN] nominal case for pricing by discount : null provided

--- src/main/java/org/supermarket/pricing/scheme/PricingByDiscountScheme.java
+++ src/main/java/org/supermarket/pricing/scheme/PricingByDiscountScheme.java
@@ -1,5 +1,6 @@
 package org.supermarket.pricing.scheme;
 
+import org.supermarket.pricing.exception.NoItemProvidedException;
 import org.supermarket.pricing.model.Item;
 
 import java.math.BigDecimal;
@@ -8,6 +9,10 @@ import java.util.List;
 public class PricingByDiscountScheme implements PricingScheme {
 
     public BigDecimal computePrice(List<Item> items) {
+
+        if(items == null){
+            throw new NoItemProvidedException(this);
+        }
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
